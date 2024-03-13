0ef873d86830fc8407012c04208d40fc8ed4324c
Wawg34345 <andrey.dar1214@gmail.com>
2024-03-13 16:50:08 +0300

    fixed logs

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,8 +1,10 @@
 <?php
-$dt = date("d-m-Y H:i:s");
-$page = $_SERVER['REQUEST_URI'];
-$url = $_SERVER['HTTP_REFERER'];
-$str = $dt . ' ' . $page . ' ' . $url;
-$log = fopen(PATH_LOG, 'a+');
-fwrite($log, $str);
-fclose($log);
+if(isset($_SERVER["HTTP_REFERER"])){
+    $dt = date("d-m-Y H:i:s");
+    $page = $_SERVER['REQUEST_URI'];
+    $url = $_SERVER['HTTP_REFERER'];
+    $str = $dt . ' ' . $page . ' ' . $url . "\n";
+    $log = fopen(PATH_LOG, 'a+');
+    fwrite($log, $str);
+    fclose($log);
+}
