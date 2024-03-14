0eda82bd4f0660c2dc1be476d4799b17fb01e325
Wawg34345 <andrey.dar1214@gmail.com>
2024-03-14 10:22:57 +0300

    fived view-log

--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,2 +1,11 @@
-<?php
+<ol>
+    <?php
+        $log_lines = file(PATH_LOG);
+        foreach($log_lines as $elem){
+            echo "<li>";
+            echo $elem;
+            echo "</li>";
+        }
+    ?>
+</ol>
 
